f56297423e4b835a66eedda750f44e745710b93b
znemmers <znemmers3@gmail.com>
2023-10-17 21:00:11 -0500

    quiz selecting both answers on click

--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -1,24 +1,49 @@
+import axios from "axios"
+import { MOVE_CLOCKWISE, 
+  MOVE_COUNTERCLOCKWISE, 
+  SET_QUIZ_INTO_STATE, 
+  SET_SELECTED_ANSWER, 
+  SET_INFO_MESSAGE,
+  INPUT_CHANGE,
+  RESET_FORM } from "./action-types"
+
+
 // ❗ You don't need to add extra action creators to achieve MVP
-export function moveClockwise() { }
+export function moveClockwise() { 
+  return {type: MOVE_CLOCKWISE} 
+}
 
-export function moveCounterClockwise() { }
+export function moveCounterClockwise() { 
+  return {type: MOVE_COUNTERCLOCKWISE}
+}
 
-export function selectAnswer() { }
+export function selectAnswer() { 
+  return {type: SET_SELECTED_ANSWER}
+}
 
-export function setMessage() { }
+export function setMessage(message) { 
+  return {type: SET_INFO_MESSAGE, payload: message}
+}
 
-export function setQuiz() { }
+export function setQuiz(quizInfo) {
+  return {type: SET_QUIZ_INTO_STATE, payload: quizInfo }
+ }
 
-export function inputChange() { }
+export function inputChange() { 
+  return {type: INPUT_CHANGE}
+}
 
-export function resetForm() { }
+export function resetForm() {
+  return {type: RESET_FORM}
+ }
 
 // ❗ Async action creators
 export function fetchQuiz() {
   return function (dispatch) {
     // First, dispatch an action to reset the quiz state (so the "Loading next quiz..." message can display)
-    // On successful GET:
+    // On successful GET: 
     // - Dispatch an action to send the obtained quiz to its state
+    // http://localhost:9000/api/quiz/next
   }
 }
 export function postAnswer() {
@@ -27,6 +52,7 @@ export function postAnswer() {
     // - Dispatch an action to reset the selected answer state
     // - Dispatch an action to set the server message to state
     // - Dispatch the fetching of the next quiz
+    // http://localhost:9000/api/quiz/new
   }
 }
 export function postQuiz() {
@@ -34,6 +60,7 @@ export function postQuiz() {
     // On successful POST:
     // - Dispatch the correct message to the the appropriate state
     // - Dispatch the resetting of the form
+    // http://localhost:9000/api/quiz/answer
   }
 }
 // ❗ On promise rejections, use log statements or breakpoints, and put an appropriate error message in state
