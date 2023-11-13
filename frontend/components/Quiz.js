cb60fa0439f1e650c56dbb65230dc88599b2fc92
znemmers <znemmers3@gmail.com>
2023-11-12 22:55:55 -0600

    answers rendering

--- frontend/components/Quiz.js
+++ frontend/components/Quiz.js
@@ -1,18 +1,16 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect} from 'react'
 import { connect } from 'react-redux'
 import { selectAnswer, setMessage, setQuiz, fetchQuiz } from '../state/action-creators'
 
 
 function Quiz(props) {
-  const {selectAnswer, setQuiz, setMessage, fetchQuiz} = props
-  
-  console.log(props)
-  
+  const {selectAnswer, setMessage, fetchQuiz} = props
+    
   useEffect(() => {
     fetchQuiz()
   }, [])
 
-  console.log("hello", props.quiz)
+  console.log(props.quiz)
   
   const handleSelect = () => {
     // need to update className to "answer selected" on click, button text changes to SELECTED -- currently selects both answers when clicked
@@ -30,29 +28,30 @@ function Quiz(props) {
     console.log('clicked')
   }
 
+  const [a, setA] = useState()
 
   return (
     <div id="wrapper">
       {
         // quiz already in state? Let's use that, otherwise render "Loading next quiz..." 
+        
         true ? (
           <>
 
             <h2>{props.quiz.question}</h2>
 
-            <div id="quizAnswers">
+            <div id="quizAnswers"> 
               <div className={`${props.ans === 1 ? 'answer selected' : 'answer'}`}>  
-              
-                {props.quiz.answers.answers}
+                  {props.quiz.answers.length ? props.quiz.answers[0].text : ''}
                 <button id='first button' onClick={handleSelect}>
                   {props.ans === 1 ? 'SELECTED' : 'Select'}
                 </button>
               </div>
 
               <div className={`${props.ans === 1 ? 'answer selected' : 'answer'}`}>
-                An elephant
+                 {props.quiz.answers.length ? props.quiz.answers[1].text : ''}
                 <button id= 'second button' onClick={handleSelectToo}>
-                {props.ans === 1 ? 'SELECTED' : 'Select'}
+                  {props.ans === 1 ? 'SELECTED' : 'Select'}
                 </button>
               </div>
             </div>
