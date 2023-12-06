e14295418aaea92f767699a54f0dae7c34ff835c
znemmers <znemmers3@gmail.com>
2023-12-05 21:40:21 -0600

    MVP completed

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { inputChange, postQuiz } from '../state/action-creators';
+import { inputChange, postQuiz, resetForm } from '../state/action-creators';
 
 export function Form(props) {
   const { inputChange, postQuiz, form } = props;
@@ -20,9 +20,9 @@ export function Form(props) {
   return (
     <form id="form" onSubmit={onSubmit}>
       <h2>Create New Quiz</h2>
-      <input maxLength={50} onChange={onChange} id="newQuestion" placeholder="Enter question" />
-      <input maxLength={50} onChange={onChange} id="newTrueAnswer" placeholder="Enter true answer" />
-      <input maxLength={50} onChange={onChange} id="newFalseAnswer" placeholder="Enter false answer" />
+      <input maxLength={50} onChange={onChange} value= {form.newQuestion} id="newQuestion" placeholder="Enter question" />
+      <input maxLength={50} onChange={onChange} value={form.newTrueAnswer} id="newTrueAnswer" placeholder="Enter true answer" />
+      <input maxLength={50} onChange={onChange} value={form.newFalseAnswer} id="newFalseAnswer" placeholder="Enter false answer" />
       <button id="submitNewQuizBtn" disabled={isSubmitDisabled}>
         Submit new quiz
       </button>
@@ -34,4 +34,4 @@ const mapStateToProps = (state) => ({
   form: state.form,
 });
 
-export default connect(mapStateToProps, { inputChange, postQuiz })(Form);
+export default connect(mapStateToProps, { inputChange, postQuiz, resetForm })(Form);
